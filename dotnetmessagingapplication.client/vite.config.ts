d80644fb41761a9bf4b734913a69a65cfb43f336
Andrew.Towell <Andrew.Towell@wisetechglobal.com>
2025-08-14 20:49:00 +1000

    Built frontend to make compatible with github pages (changed base path in vite.config.ts)

--- dotnetmessagingapplication.client/vite.config.ts
+++ dotnetmessagingapplication.client/vite.config.ts
@@ -39,6 +39,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/dot-net-messaging-application/',
     plugins: [plugin()],
     resolve: {
         alias: {
