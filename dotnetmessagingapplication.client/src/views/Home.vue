2355f9c6f537246333d5afb28dacaff586c4cd73
Andrew.Towell <Andrew.Towell@wisetechglobal.com>
2025-08-16 14:53:36 +1000

    Added Login UI. No functionality

--- dotnetmessagingapplication.client/src/views/Home.vue
+++ dotnetmessagingapplication.client/src/views/Home.vue
@@ -1,3 +1,27 @@
+<script lang="ts">
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+        data() {
+            return {
+
+            };
+        },
+
+        // lifecycle hook (called on mount)
+        async mounted() {
+
+        },
+
+        methods: {
+
+        }
+    });
+</script>
+
 <template>
     <h1>This is home</h1>
 </template>
+
+<style scoped>
+</style>
--- dotnetmessagingapplication.client/src/views/Login.vue
+++ dotnetmessagingapplication.client/src/views/Login.vue
@@ -1,3 +1,63 @@
+<script setup lang="ts">
+    // import neccessary components
+    import Widget from "../components/Widget.vue"
+</script>
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+        data() {
+            return {
+
+            };
+        },
+
+        // lifecycle hook (called on mount)
+        async mounted() {
+
+        },
+
+        methods: {
+
+        }
+    });
+</script>
+
 <template>
-    <h1>This is login</h1>
+    <div id="login">
+        <Widget id="login-widget" title="Log In">
+            <div class="login-input">
+                <label for="username-input">Username: </label>
+                <input id="username-input" placeholder="Username..."/>
+            </div>
+            <div class="login-input">
+                <label for="password-input">Password: </label>
+                <input id="password-input" placeholder="Password..."/>
+            </div>
+            <button id="login-button">Log in</button>
+        </Widget>
+    </div>
 </template>
+
+<style scoped>
+    #login {
+        margin: auto;
+        display: flex;
+    }
+
+    #login-widget {
+        width: 300px;
+        margin: auto;
+        margin-top: 15%;
+        align-content: center;
+    }
+
+    .login-input {
+        margin: 5px;
+    }
+
+    #login-button {
+        margin-top: 10px;
+    }
+</style>
