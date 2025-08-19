3491c104edde3d41a63b687a058f844c5d5231ea
ykjuts <yixin.jiang@student.uts.edu.au>
2025-08-19 15:50:09 +1000

    migration

--- DotNetMessagingApplication.Server/Migrations/20250819054944_AddEntitiesToDbset.Designer.cs
+++ DotNetMessagingApplication.Server/Migrations/20250819054944_AddEntitiesToDbset.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DotNetMessagingApplication.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DotNetMessagingApplication.Server.Migrations
 {
     [DbContext(typeof(MessagingAppContext))]
-    partial class MessagingAppContextModelSnapshot : ModelSnapshot
+    [Migration("20250819054944_AddEntitiesToDbset")]
+    partial class AddEntitiesToDbset
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.8");
---
+++ DotNetMessagingApplication.Server/Migrations/20250819054944_AddEntitiesToDbset.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNetMessagingApplication.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEntitiesToDbset : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Relationship_Users_OtherPersonId",
+                table: "Relationship");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Relationship_Users_UserId",
+                table: "Relationship");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Relationship",
+                table: "Relationship");
+
+            migrationBuilder.RenameTable(
+                name: "Relationship",
+                newName: "Relationships");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Relationship_OtherPersonId",
+                table: "Relationships",
+                newName: "IX_Relationships_OtherPersonId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Users",
+                type: "TEXT",
+                maxLength: 5,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ParentId",
+                table: "Users",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Relationships",
+                table: "Relationships",
+                columns: new[] { "UserId", "OtherPersonId" });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    MessageId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    SenderId = table.Column<int>(type: "INTEGER", nullable: false),
+                    RecipientId = table.Column<int>(type: "INTEGER", nullable: false),
+                    MessageBody = table.Column<string>(type: "TEXT", nullable: false),
+                    TimeSent = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.MessageId);
+                    table.ForeignKey(
+                        name: "FK_Messages_Users_RecipientId",
+                        column: x => x.RecipientId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Messages_Users_SenderId",
+                        column: x => x.SenderId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_ParentId",
+                table: "Users",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_RecipientId",
+                table: "Messages",
+                column: "RecipientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_SenderId",
+                table: "Messages",
+                column: "SenderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Relationships_Users_OtherPersonId",
+                table: "Relationships",
+                column: "OtherPersonId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Relationships_Users_UserId",
+                table: "Relationships",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Users_ParentId",
+                table: "Users",
+                column: "ParentId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Relationships_Users_OtherPersonId",
+                table: "Relationships");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Relationships_Users_UserId",
+                table: "Relationships");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Users_ParentId",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_ParentId",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Relationships",
+                table: "Relationships");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ParentId",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Relationships",
+                newName: "Relationship");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Relationships_OtherPersonId",
+                table: "Relationship",
+                newName: "IX_Relationship_OtherPersonId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Relationship",
+                table: "Relationship",
+                columns: new[] { "UserId", "OtherPersonId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Relationship_Users_OtherPersonId",
+                table: "Relationship",
+                column: "OtherPersonId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Relationship_Users_UserId",
+                table: "Relationship",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
