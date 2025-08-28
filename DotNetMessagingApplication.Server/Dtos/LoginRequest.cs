6e6f35025a00f0082e522bd643ef21abdb8040af
ykjuts <yixin.jiang@student.uts.edu.au>
2025-08-29 01:46:30 +1000

    fixed json error

--- DotNetMessagingApplication.Server/Dtos/LoginRequest.cs
+++ DotNetMessagingApplication.Server/Dtos/LoginRequest.cs
@@ -6,8 +6,8 @@ namespace DotNetMessagingApplication.Server.Dtos;
 public class LoginRequest
 {
 	[JsonProperty("emailOrUsername")]
-	public string EmailOrUsername { get; } = null!;
+	public string EmailOrUsername { get; set; } = null!;
 
 	[JsonProperty("password")]
-	public string Password { get; } = null!;
+	public string Password { get; set; } = null!;
 }
