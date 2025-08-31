75604674f652da5f3493c290a46b8b8bbb60cedf
Kim Do <Kim.Do@wisetechglobal.com>
2025-09-01 08:49:19 +1000

    controllers - need to support websockets

--- DotNetMessagingApplication.Server/Controllers/ChatController.cs
+++ DotNetMessagingApplication.Server/Controllers/ChatController.cs
@@ -1,39 +1,12 @@
-﻿using DotNetMessagingApplication.Server.Data.Dtos;
+﻿using DotNetMessagingApplication.Server.Dtos;
 using DotNetMessagingApplication.Server.Data.Models;
 using DotNetMessagingApplication.Server.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetMessagingApplication.Server.Controllers
 {
-	public class ChatController : Controller
-	{
-		private readonly IMessageService _messageService;
+    public class ChatController : Controller
+    {
 
-		public ChatController(IMessageService messageService) {
-			_messageService = messageService;
-		}
-
-		[HttpPost("SendMessage")]
-		public IActionResult SendMessage([FromBody] SendMessageDto sentMessage)
-		{
-			if (sentMessage is null)
-			{
-				return BadRequest("Message data is missing");
-			}
-
-
-			// get chat by id
-			//Chat chat = 
-
-			Message message = new()
-			{
-				MessageBody = sentMessage.Message,
-				SenderId = sentMessage.SenderId,
-				TimeSent = sentMessage.TimeStamp,
-
-			};
-
-			return Ok(1);
-		}
-	}
+    }
 }
---
+++ DotNetMessagingApplication.Server/Controllers/MessageController.cs
@@ -0,0 +1,70 @@
+﻿using DotNetMessagingApplication.Server.Data.Models;
+using DotNetMessagingApplication.Server.Dtos;
+using DotNetMessagingApplication.Server.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetMessagingApplication.Server.Controllers;
+
+[ApiController]
+[Route("api/controllers")]
+	public class MessageController : ControllerBase
+	{
+		private readonly IMessageService _messageService;
+
+		public MessageController(IMessageService messageService) {
+			_messageService = messageService;
+		}
+
+		[HttpPost("SendMessage")]
+		public IActionResult SendMessage([FromBody] SendMessageDto sentMessage)
+		{
+			if (sentMessage is null)
+			{
+				return BadRequest("Message data is missing");
+			}
+
+
+			// get chat by id
+			//Chat chat = 
+
+			Message message = new()
+			{
+				MessageBody = sentMessage.Message,
+				SenderId = sentMessage.SenderId,
+				TimeSent = sentMessage.TimeStamp,
+			};
+
+			return Ok(1);
+		}
+
+		[HttpDelete("DeleteMessage/{messageId}")]
+		public IActionResult DeleteMessage([FromBody] DeleteMessageDto deletedMessage)
+		{
+			if (deletedMessage is null)
+			{
+				return BadRequest("You sure there's a message ?? Couldn't find one D:");
+			}
+
+			//var chat = _messageService
+
+			//if (messageId < 0)
+			//{
+			//	return BadRequest("Invalid message ID :(");
+			//}
+
+			//_messageService.DeleteMessage(messageId);
+			return Ok(1);
+		}
+
+		[HttpPut("EditMessage/{messageId}")]
+		public IActionResult EditMessage(int messageId)
+		{
+			if (messageId < 0)
+			{
+				return BadRequest("Invalid message id D:");
+			}
+
+			_messageService.UpdateMessage(messageId);
+			return Ok(1);
+		}
+	}
