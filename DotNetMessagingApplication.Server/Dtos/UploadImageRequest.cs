6b8989fb3740631975b678510933baa46a078dfd
ykjuts <yixin.jiang@student.uts.edu.au>
2025-09-02 20:20:33 +1000

    yeah

--- DotNetMessagingApplication.Server/Dtos/UploadImageRequest.cs
+++ DotNetMessagingApplication.Server/Dtos/UploadImageRequest.cs
@@ -5,7 +5,7 @@ namespace DotNetMessagingApplication.Server.Dtos
     public class UploadImageRequest
     {
         [FromForm(Name = "image")]
-        public IFormFile Image { get; set; }
+        public IFormFile Image { get; set; } = null!;
 
         [FromForm(Name = "chatId")]
         public int ChatId { get; set; }
